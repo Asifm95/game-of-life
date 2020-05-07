812ecab5e0440324d9ed7b833f20db842402dfd7
Asif Mohammed <asifm2882@gmail.com>
2020-05-08 04:37:23 +0530

    Updates

---
+++ precache-manifest.ae21d5f83bb9636bc72bc6bf556f5913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e9cb216320cf0a5ed2220e0ff021e5",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "d314b2894c3190cae49f",
+    "url": "/game-of-life/static/css/main.c96df019.chunk.css"
+  },
+  {
+    "revision": "c58ddd7b1a23cfbf0601",
+    "url": "/game-of-life/static/js/2.3fe1ed2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.3fe1ed2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d314b2894c3190cae49f",
+    "url": "/game-of-life/static/js/main.d8144a54.chunk.js"
+  },
+  {
+    "revision": "51df4f6466fc46c339a1",
+    "url": "/game-of-life/static/js/runtime-main.622907b2.js"
+  }
+]);
