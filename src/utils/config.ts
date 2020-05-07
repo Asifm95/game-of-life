f17b314a4779f0d423476b4f5030bbbc705a2c72
Asif Mohammed <asifm2882@gmail.com>
2020-05-08 04:22:30 +0530

    02-Life: update cell padding

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,21 +2,20 @@ export const config = {
   board: {
     row: 50,
     column: 50,
+    randomize: true,
     padding: {
-      verticalPadding: 6,
-      hoirizontalPadding: 4,
-      smScreenVerticalPadding: 6,
-      smScreenhoirizontalPadding: 2,
+      verticalPadding: 3,
+      hoirizontalPadding: 0,
     },
   },
   cells: {
-    size: 15,
-    aliveColor: "#212121",
-    deadColor: "#f1f1f1",
-    border: "solid 1px black",
+    size: 20,
+    aliveColor: "#dc7373",
+    deadColor: "#212121",
+    border: "solid 1px #0f0f0f",
   },
   controls: {
-    speed: 100,
+    speed: 25,
     gameDescription:
       "The game is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine. Simply click on the grid to set intial state and click play!",
     startBtnColor: "#a3dc63",
